9c70ee2a324a11388dfb3340d61c116aab01ff40
Ishaan Kurji <ishaankurji@ishaans-mbp.lan>
2019-02-19 16:44:12 -0600

    second commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Ishaan Kurji on 2/19/19.
+//  Copyright © 2019 Ishaan Kurji. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
